dd0267a89a02fd958d40e623648aae976a06b4ce
Radek Kehar <kehar@oksystem.cz>
2022-03-30 09:55:03 +0200

    add fontawesome

---
+++ src/fontawesome.js
@@ -0,0 +1,12 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import your icons
+import { faEquals, faPlus, faMinus, faGreaterThan, faLessThan } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+    faEquals,
+    faPlus,
+    faMinus,
+    faGreaterThan,
+    faLessThan
+);
